3e6c74dc134f888b16c36e106d6861573a105e93
Joe Jeskiewicz <artexercise@yahoo.com>
2016-08-27 23:27:58 -0400

    Second Chapter

--- Hackpad.py
+++ Hackpad.py
@@ -38,4 +38,55 @@ class Hackpad:
         """
         return abs(inArg)
 
-    
+    @staticmethod
+    def Power(inArg):
+        """
+           High School Hack 
+            H:
+            while var_b < input {
+            	var_b++;
+            	var_a = var_a + input;
+            }
+        """
+        a = 0
+        b = 0
+        while b < inArg:
+        	b = b + 1
+        	a = a + inArg
+        return a
+
+    @staticmethod
+    def Power_2(inArg):
+        """
+           High School Hack 
+            H:
+            pow(input, 2);
+        """
+        return inArg**2
+
+    @staticmethod
+    def Length(inArg):
+        """
+           High School Hack 
+            H:
+            foreach var_b in input {
+				var_a++;
+            }
+            return var_a;
+        """
+        return len(inArg)
+
+    @staticmethod
+    def Push_it(inArg):
+        """
+           High School Hack 
+            H:
+            var_a = [];
+            while var_b < input {
+				var_a.push(var_b);
+				var_b++;
+            }
+            return var_a;
+        """
+        return range(inArg)
+
--- test_Hackpad.py
+++ test_Hackpad.py
@@ -24,6 +24,30 @@ def test_Absolute_2():
     if not testitem_FunctionTest(H.Absolute_2, 0, 0): return
     print("Passed: Absolute_2")
 
+def test_Power():
+    if not testitem_FunctionTest(H.Power, 2, 4): return
+    if not testitem_FunctionTest(H.Power, 3, 9): return
+    if not testitem_FunctionTest(H.Power, 4, 16): return
+    print("Passed: Power")
+
+def test_Power_2():
+    if not testitem_FunctionTest(H.Power_2, 2, 4): return
+    if not testitem_FunctionTest(H.Power_2, 3, 9): return
+    if not testitem_FunctionTest(H.Power_2, 4, 16): return
+    print("Passed: Power_2")
+
+def test_Length():
+    if not testitem_FunctionTest(H.Length, [3,8], 2): return
+    if not testitem_FunctionTest(H.Length, [4,5,3], 3): return
+    if not testitem_FunctionTest(H.Length, [6], 1): return
+    print("Passed: Length")
+
+def test_Push_it():
+    if not testitem_FunctionTest(H.Push_it, 5, [0,1,2,3,4]): return
+    if not testitem_FunctionTest(H.Push_it, 2, [0,1]): return
+    if not testitem_FunctionTest(H.Push_it, 0, []): return
+    print("Passed: Push_it")
+
 def testitem_FunctionTest(inFunc, inArg, outResult):
     try:
         assert inFunc(inArg)==outResult
@@ -39,3 +63,8 @@ test_Increment_me()
 test_Positive()
 test_Absolute()
 test_Absolute_2()
+
+test_Power()
+test_Power_2()
+test_Length()
+test_Push_it()
